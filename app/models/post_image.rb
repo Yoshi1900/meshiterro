bc2412900dfc876fb9687be361efe49bc75cedc9
EC2 Default User <ec2-user@ip-172-31-34-244.ap-northeast-1.compute.internal>
2024-09-12 06:19:28 +0000

    [finish]5-10

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,7 +1,5 @@
-class Postimage < ApplicationRecord
+class PostImage < ApplicationRecord
   
   has_one_attached :image
-  
   belongs_to :user
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'postimages/new'
-  get 'postimages/index'
-  get 'postimages/show'
+
   devise_for :users
   get root :to => 'homes#top'
   get 'about' => 'homes#about'
-  
-  resource :post_images, only: [:new, :index, :show]
+
+  resources :post_images, only: [:new, :create, :index, :show]
 end
