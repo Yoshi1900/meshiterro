957f7b1634ee3af09229afce003b60bb90855d76
EC2 Default User <ec2-user@ip-172-31-34-244.ap-northeast-1.compute.internal>
2024-09-14 00:58:06 +0000

    [finish]5-18

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,15 @@ Rails.application.routes.draw do
   get root :to => 'homes#top'
   get 'about' => 'homes#about'
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create]
+  end
+
   resources :users, only: [:show, :edit, :update]
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+
+
+
+
 end
